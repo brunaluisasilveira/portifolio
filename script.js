13ef41dc470f929002f8965f80b9fc20616d5c49
Bruna Luisa Silveira <brunnaluiisa.bl@gmail.com>
2021-07-23 11:00:32 -0300

    atualização

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var hamburguer = document.querySelector(".hamburguer");
 
-hamburguer.addEventListener("click", function(){
+hamburguer.addEventListener("click", function() {
     document.querySelector(".sidebar").classList.toggle("show-menu");
     document.querySelector(".container").classList.toggle("show-menu");
 });
- 
